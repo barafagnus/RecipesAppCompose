076672a1cb4948472203726f44c152b5db0a4911
barafagnus <barafagnus@gmail.com>
2026-03-11 19:44:24 +0700

    Impl base structure of app

--- app/src/main/java/ru/vysokov/recipesappcompose/MainActivity.kt
+++ app/src/main/java/ru/vysokov/recipesappcompose/MainActivity.kt
@@ -8,9 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ru.vysokov.recipesappcompose.ui.theme.RecipesAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +17,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipesAppComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+                    Text(
+                        text = "Recipes App",
+                        modifier = Modifier.padding(paddingValues)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecipesAppComposeTheme {
-        Greeting("Android")
     }
 }
