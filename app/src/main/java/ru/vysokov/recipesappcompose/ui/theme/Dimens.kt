3d1251677770bdbf3bdd3ca0e821ec437a8a6c4d
barafagnus <barafagnus@gmail.com>
2026-03-16 01:01:27 +0700

    RCA-06 fix Type.kt, add colorscheme and dimens

---
+++ app/src/main/java/ru/vysokov/recipesappcompose/ui/theme/Dimens.kt
@@ -0,0 +1,39 @@
+package ru.vysokov.recipesappcompose.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val titleCardContentPadding = 10.dp
+
+    val cardOuterPadding = 16.dp
+    val cardContentPadding = 8.dp
+    val buttonContentPadding = 10.dp
+    val buttonHorizontalSpacing = 4.dp
+
+    val innerContentPadding = 6.dp
+    val titlePadding = 16.dp
+
+    val listItemPadding = 12.dp
+    val toDividerSpacing = 8.dp
+
+    val headerImageHeight = 224.dp
+
+    val categoryCardImageWidth = 156.dp
+    val categoryCardImageHeight = 130.dp
+
+    val recipeCardImageWidth = 100.dp
+    val recipeCardImageHeight = 328.dp
+
+    val favoritesButtonIconWidth = 21.dp
+    val favoritesButtonIconHeight = 18.dp
+
+    val favoritesIconWidth = 35.dp
+    val favoritesIconHeight = 30.dp
+
+    val sliderTrackHeight = 16.dp
+    val sliderLeverWidth = 8.dp
+    val sliderLeverHeight = 30.dp
+
+    val placeHolderWidth = 520.dp
+    val placeHolderHeight = 295.dp
+}
