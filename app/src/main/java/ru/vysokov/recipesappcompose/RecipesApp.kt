cd296079f29705c3681e7124c2a2701f67366253
barafagnus <barafagnus@gmail.com>
2026-03-20 01:36:43 +0700

    RCA-08 impl base navigation

--- app/src/main/java/ru/vysokov/recipesappcompose/RecipesApp.kt
+++ app/src/main/java/ru/vysokov/recipesappcompose/RecipesApp.kt
@@ -1,22 +1,48 @@
 package ru.vysokov.recipesappcompose
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import ru.vysokov.recipesappcompose.ui.navigation.BottomNavigation
 import ru.vysokov.recipesappcompose.ui.theme.RecipesAppComposeTheme
 
 @Composable
 fun RecipesApp() {
+    var currentScreen by remember { mutableStateOf(ScreenId.CATEGORIES) }
+
     RecipesAppComposeTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
-            Text(
-                text = "Recipes App",
-                modifier = Modifier.padding(paddingValues)
-            )
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize(),
+            bottomBar = {
+                BottomNavigation(
+                    onCategoriesClick = { currentScreen = ScreenId.CATEGORIES },
+                    onFavoritesClick = { currentScreen = ScreenId.FAVORITES }
+                )
+            }
+        ) { paddingValues ->
+            when (currentScreen) {
+                ScreenId.CATEGORIES -> ScreenId.FAVORITES
+                ScreenId.FAVORITES -> ScreenId.CATEGORIES
+            }
+
+            Box(modifier = Modifier.fillMaxSize()) {
+                Text(modifier = Modifier
+                    .padding(paddingValues)
+                    .align(Alignment.Center),
+                    text = currentScreen.toString()
+                )
+            }
         }
     }
 }
