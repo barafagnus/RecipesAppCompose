7709080a8948aff0f8dd581d8c50c8a7d3ef7467
barafagnus <barafagnus@gmail.com>
2026-05-15 23:20:10 +0700

    RCA-24 some fix

--- app/src/main/java/ru/vysokov/recipesappcompose/ui/favorites/FavoritesScreen.kt
+++ app/src/main/java/ru/vysokov/recipesappcompose/ui/favorites/FavoritesScreen.kt
@@ -1,14 +1,20 @@
 package ru.vysokov.recipesappcompose.ui.favorites
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import ru.vysokov.recipesappcompose.R
 import ru.vysokov.recipesappcompose.core.ui.ScreenHeader
 import ru.vysokov.recipesappcompose.data.model.RecipeDto
@@ -31,17 +37,33 @@ fun FavoritesScreen(
             imageModel = R.drawable.bcg_favorites
         )
 
-        LazyColumn(
-            modifier = Modifier.weight(1f),
-            verticalArrangement = Arrangement.spacedBy(Dimens.paddingMedium),
-            contentPadding = PaddingValues(Dimens.paddingMedium)
-        ) {
-            items(items = favoritesRecipes, key = { it.id }) { recipe ->
-                val recipe = recipe.toUiModel()
-                RecipeItem(
-                    model = recipe,
-                    onClick = { onRecipeClick(recipe.id, recipe) }
+        if (favoritesRecipes.isEmpty()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = Dimens.paddingMedium),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    textAlign = TextAlign.Center,
+                    text = stringResource(R.string.empty_favorites_text),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
                 )
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.spacedBy(Dimens.paddingMedium),
+                contentPadding = PaddingValues(Dimens.paddingMedium)
+            ) {
+                items(items = favoritesRecipes, key = { it.id }) { recipe ->
+                    val recipeUiModel = recipe.toUiModel()
+                    RecipeItem(
+                        model = recipeUiModel,
+                        onClick = { onRecipeClick(recipeUiModel.id, recipeUiModel) }
+                    )
+                }
             }
         }
     }
