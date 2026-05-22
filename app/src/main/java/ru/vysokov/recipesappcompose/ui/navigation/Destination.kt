e6361040230984ef72327ecb78888ecee9f9d477
barafagnus <barafagnus@gmail.com>
2026-05-22 15:03:01 +0700

    RCA-26 categories vm, route rework

--- app/src/main/java/ru/vysokov/recipesappcompose/ui/navigation/Destination.kt
+++ app/src/main/java/ru/vysokov/recipesappcompose/ui/navigation/Destination.kt
@@ -1,12 +1,16 @@
 package ru.vysokov.recipesappcompose.ui.navigation
 
 sealed class Destination(val route: String) {
-    object Categories: Destination("categories")
-    object Recipes: Destination("recipes/{categoryId}") {
-        fun createRoute(categoryId: Int) = "recipes/$categoryId"
+    object Categories : Destination("categories")
+    object Recipes :
+        Destination("recipes/{categoryId}?title={categoryTitle}&imageUrl={categoryImageUrl}") {
+        fun createRoute(categoryId: Int, categoryTitle: String, categoryImageUrl: String) =
+            "recipes/$categoryId?title=$categoryTitle&imageUrl=$categoryImageUrl"
     }
-    object RecipeDetails: Destination("recipe/{recipeId}") {
+
+    object RecipeDetails : Destination("recipe/{recipeId}") {
         fun createRoute(recipeId: Int) = "recipe/$recipeId"
     }
-    object Favorites: Destination("favorites")
+
+    object Favorites : Destination("favorites")
 }
