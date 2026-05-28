478a896141bcbe617410ae3c5c3c24838f61c707
barafagnus <barafagnus@gmail.com>
2026-05-28 14:46:55 +0700

    RCA-31 api request by HttpUrlConnection

--- app/src/main/java/ru/vysokov/recipesappcompose/MainActivity.kt
+++ app/src/main/java/ru/vysokov/recipesappcompose/MainActivity.kt
@@ -2,12 +2,19 @@ package ru.vysokov.recipesappcompose
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import kotlinx.serialization.json.Json
+import ru.vysokov.recipesappcompose.core.Constants
+import ru.vysokov.recipesappcompose.data.model.CategoryDto
+import java.net.HttpURLConnection
+import java.net.URL
+
 
 class MainActivity : ComponentActivity() {
     private var deepLinkIntent by mutableStateOf<Intent?>(null)
@@ -18,6 +25,29 @@ class MainActivity : ComponentActivity() {
         intent?.data?.let { _ ->
             deepLinkIntent = intent
         }
+        Log.i("!!!", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}")
+
+        val thread = Thread({
+            Log.i("!!!", "Выполняю запрос на потоке:: ${Thread.currentThread().name}")
+
+            val url = URL(Constants.RECIPES_BASE_URL + "/category")
+            val conn = url.openConnection() as HttpURLConnection
+            try {
+                conn.connect()
+                val body = conn.inputStream.bufferedReader().readText()
+                val categories = Json.decodeFromString<List<CategoryDto>>(body)
+
+                Log.i("!!!", "responseCode: ${conn.responseCode}")
+                Log.i("!!!", "responseMessage: ${conn.responseMessage}")
+
+                Log.i("!!!", "categories size: ${categories.size}")
+                Log.i("!!!", "categories: ${categories.map { it.title }}")
+            } finally {
+                conn.disconnect()
+            }
+        }, "RecipesApiThread")
+
+        thread.start()
 
         enableEdgeToEdge()
         setContent {
