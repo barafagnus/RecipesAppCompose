9c4a6d589b729278daa9c23c82620074d7b43ded
barafagnus <barafagnus@gmail.com>
2026-06-07 11:15:08 +0700

    RCA-37 impl RecipeImage component

---
+++ app/src/main/java/ru/vysokov/recipesappcompose/core/ui/RecipeImage.kt
@@ -0,0 +1,71 @@
+package ru.vysokov.recipesappcompose.core.ui
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import coil3.size.Scale
+import ru.vysokov.recipesappcompose.R
+import ru.vysokov.recipesappcompose.ui.theme.Dimens
+
+@Composable
+fun RecipeImage(
+    modifier: Modifier = Modifier,
+    imageUrl: String,
+    contentDescription: String,
+    contentScale: ContentScale = ContentScale.Crop
+) {
+    var isLoading by remember { mutableStateOf(false) }
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(Dimens.recipeCardImageWidth),
+        contentAlignment = Alignment.Center
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentScale = contentScale,
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(imageUrl)
+                .crossfade(true)
+                .crossfade(300)
+                .size(200, 200)
+                .scale(Scale.FILL)
+                .build(),
+            contentDescription = contentDescription,
+            placeholder = painterResource(R.drawable.img_placeholder),
+            error = painterResource(R.drawable.img_error),
+            onLoading = { isLoading = true },
+            onSuccess = { isLoading = false },
+            onError = { isLoading = false }
+        )
+
+        AnimatedVisibility(
+            visible = isLoading,
+            enter = EnterTransition.None,
+            exit = fadeOut(animationSpec = tween(durationMillis = 300))
+        ) {
+            CircularProgressIndicator()
+        }
+    }
+
+}
