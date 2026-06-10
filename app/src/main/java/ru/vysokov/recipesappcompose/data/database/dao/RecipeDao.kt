45524c279bce6bfafa8907a245b489b457ed340a
barafagnus <barafagnus@gmail.com>
2026-06-11 00:05:52 +0700

    RCA-40 impl dao methods

--- app/src/main/java/ru/vysokov/recipesappcompose/data/database/dao/RecipeDao.kt
+++ app/src/main/java/ru/vysokov/recipesappcompose/data/database/dao/RecipeDao.kt
@@ -1,6 +1,8 @@
 package ru.vysokov.recipesappcompose.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 import ru.vysokov.recipesappcompose.data.database.entity.RecipeEntity
@@ -9,4 +11,16 @@ import ru.vysokov.recipesappcompose.data.database.entity.RecipeEntity
 interface RecipeDao {
     @Query("SELECT * FROM recipes")
     fun getAllRecipes(): Flow<List<RecipeEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipes(recipes: List<RecipeEntity>)
+
+    @Query("SELECT * FROM recipes WHERE categoryId = :categoryId")
+    fun getRecipesByCategory(categoryId: Int): Flow<List<RecipeEntity>>
+
+    @Query("SELECT * FROM recipes WHERE id = :recipeId")
+    fun getRecipeById(recipeId: Int): Flow<RecipeEntity?>
+
+    @Query("SELECT * FROM recipes WHERE id IN (:recipeIds)")
+    fun getRecipesByIds(recipeIds: List<Int>): Flow<List<RecipeEntity>>
 }
