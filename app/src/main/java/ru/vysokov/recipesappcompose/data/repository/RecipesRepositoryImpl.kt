3df2f768be10da7f18f13e26f9b6f13f3b79f492
barafagnus <barafagnus@gmail.com>
2026-06-14 16:02:18 +0700

    RCA-42 getRecipe caching

--- app/src/main/java/ru/vysokov/recipesappcompose/data/repository/RecipesRepositoryImpl.kt
+++ app/src/main/java/ru/vysokov/recipesappcompose/data/repository/RecipesRepositoryImpl.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import ru.vysokov.recipesappcompose.core.network.api.RecipesApiService
 import ru.vysokov.recipesappcompose.data.database.RecipesDatabase
 import ru.vysokov.recipesappcompose.data.database.entity.toCategoryDto
--- app/src/main/java/ru/vysokov/recipesappcompose/features/details/presentation/RecipeDetailsViewModel.kt
+++ app/src/main/java/ru/vysokov/recipesappcompose/features/details/presentation/RecipeDetailsViewModel.kt
@@ -8,7 +8,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import ru.vysokov.recipesappcompose.core.Constants
